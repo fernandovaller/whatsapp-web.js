47acb60dcc65469ebb3649d2429a1eebdee8bd39
Fernando Valler <fernandovaller@gmail.com>
2024-01-31 11:23:13 -0300

    adiciona o log

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 const axios = require('axios');
 const qrcode = require('qrcode');
+const fs = require('fs');
 
 const port = process.env.PORT || 8000;
 
@@ -17,7 +18,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-
 const client = new Client({
     authStrategy: new LocalAuth(),
     // proxyAuthentication: { username: 'username', password: 'password' },
@@ -29,88 +29,14 @@ const client = new Client({
 
 client.initialize();
 
-
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-
 client.on('message', async msg => {
     console.log('MESSAGE RECEIVED', msg);
 
-    if (msg.body === '!ping reply') {
-        // Send a new message as a reply to the current one
-        msg.reply('pong');
-
-    } else if (msg.body === '!ping') {
-        // Send a new message to the same chat
-        client.sendMessage(msg.from, 'pong');
-
-    } else if (msg.body.startsWith('!sendto ')) {
-        // Direct send a new message to specific id
-        let number = msg.body.split(' ')[1];
-        let messageIndex = msg.body.indexOf(number) + number.length;
-        let message = msg.body.slice(messageIndex, msg.body.length);
-        number = number.includes('@c.us') ? number : `${number}@c.us`;
-        let chat = await msg.getChat();
-        chat.sendSeen();
-        client.sendMessage(number, message);
-
-    } else if (msg.body.startsWith('!subject ')) {
-        // Change the group subject
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newSubject = msg.body.slice(9);
-            chat.setSubject(newSubject);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!echo ')) {
-        // Replies with the same message
-        msg.reply(msg.body.slice(6));
-    } else if (msg.body.startsWith('!desc ')) {
-        // Change the group description
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            let newDescription = msg.body.slice(6);
-            chat.setDescription(newDescription);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body === '!leave') {
-        // Leave the group
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            chat.leave();
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body.startsWith('!join ')) {
-        const inviteCode = msg.body.split(' ')[1];
-        try {
-            await client.acceptInvite(inviteCode);
-            msg.reply('Joined the group!');
-        } catch (e) {
-            msg.reply('That invite code seems to be invalid.');
-        }
-    } else if (msg.body === '!groupinfo') {
-        let chat = await msg.getChat();
-        if (chat.isGroup) {
-            msg.reply(`
-                *Group Details*
-                Name: ${chat.name}
-                Description: ${chat.description}
-                Created At: ${chat.createdAt.toString()}
-                Created By: ${chat.owner.user}
-                Participant count: ${chat.participants.length}
-            `);
-        } else {
-            msg.reply('This command can only be used in a group!');
-        }
-    } else if (msg.body === '!chats') {
-        const chats = await client.getChats();
-        client.sendMessage(msg.from, `The bot has ${chats.length} chats open.`);
-    } else if (msg.body === '!info') {
+    if (msg.body === '!info') {
         let info = client.info;
         client.sendMessage(msg.from, `
             *Connection info*
@@ -118,129 +44,6 @@ client.on('message', async msg => {
             My number: ${info.wid.user}
             Platform: ${info.platform}
         `);
-    } else if (msg.body === '!mediainfo' && msg.hasMedia) {
-        const attachmentData = await msg.downloadMedia();
-        msg.reply(`
-            *Media info*
-            MimeType: ${attachmentData.mimetype}
-            Filename: ${attachmentData.filename}
-            Data (length): ${attachmentData.data.length}
-        `);
-    } else if (msg.body === '!quoteinfo' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-
-        quotedMsg.reply(`
-            ID: ${quotedMsg.id._serialized}
-            Type: ${quotedMsg.type}
-            Author: ${quotedMsg.author || quotedMsg.from}
-            Timestamp: ${quotedMsg.timestamp}
-            Has Media? ${quotedMsg.hasMedia}
-        `);
-    } else if (msg.body === '!resendmedia' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-        if (quotedMsg.hasMedia) {
-            const attachmentData = await quotedMsg.downloadMedia();
-            client.sendMessage(msg.from, attachmentData, { caption: 'Here\'s your requested media.' });
-        }
-        if (quotedMsg.hasMedia && quotedMsg.type === 'audio') {
-            const audio = await quotedMsg.downloadMedia();
-            await client.sendMessage(msg.from, audio, { sendAudioAsVoice: true });
-        }
-    } else if (msg.body === '!isviewonce' && msg.hasQuotedMsg) {
-        const quotedMsg = await msg.getQuotedMessage();
-        if (quotedMsg.hasMedia) {
-            const media = await quotedMsg.downloadMedia();
-            await client.sendMessage(msg.from, media, { isViewOnce: true });
-        }
-    } else if (msg.body === '!location') {
-        // only latitude and longitude
-        await msg.reply(new Location(37.422, -122.084));
-        // location with name only
-        await msg.reply(new Location(37.422, -122.084, { name: 'Googleplex' }));
-        // location with address only
-        await msg.reply(new Location(37.422, -122.084, { address: '1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA' }));
-        // location with name, address and url
-        await msg.reply(new Location(37.422, -122.084, { name: 'Googleplex', address: '1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA', url: 'https://google.com' }));
-    } else if (msg.location) {
-        msg.reply(msg.location);
-    } else if (msg.body.startsWith('!status ')) {
-        const newStatus = msg.body.split(' ')[1];
-        await client.setStatus(newStatus);
-        msg.reply(`Status was updated to *${newStatus}*`);
-    } else if (msg.body === '!mention') {
-        const contact = await msg.getContact();
-        const chat = await msg.getChat();
-        chat.sendMessage(`Hi @${contact.number}!`, {
-            mentions: [contact]
-        });
-    } else if (msg.body === '!delete') {
-        if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            if (quotedMsg.fromMe) {
-                quotedMsg.delete(true);
-            } else {
-                msg.reply('I can only delete my own messages');
-            }
-        }
-    } else if (msg.body === '!pin') {
-        const chat = await msg.getChat();
-        await chat.pin();
-    } else if (msg.body === '!archive') {
-        const chat = await msg.getChat();
-        await chat.archive();
-    } else if (msg.body === '!mute') {
-        const chat = await msg.getChat();
-        // mute the chat for 20 seconds
-        const unmuteDate = new Date();
-        unmuteDate.setSeconds(unmuteDate.getSeconds() + 20);
-        await chat.mute(unmuteDate);
-    } else if (msg.body === '!typing') {
-        const chat = await msg.getChat();
-        // simulates typing in the chat
-        chat.sendStateTyping();
-    } else if (msg.body === '!recording') {
-        const chat = await msg.getChat();
-        // simulates recording audio in the chat
-        chat.sendStateRecording();
-    } else if (msg.body === '!clearstate') {
-        const chat = await msg.getChat();
-        // stops typing or recording in the chat
-        chat.clearState();
-    } else if (msg.body === '!jumpto') {
-        if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            client.interface.openChatWindowAt(quotedMsg.id._serialized);
-        }
-    } else if (msg.body === '!buttons') {
-        let button = new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer');
-        client.sendMessage(msg.from, button);
-    } else if (msg.body === '!list') {
-        let sections = [{ title: 'sectionTitle', rows: [{ title: 'ListItem1', description: 'desc' }, { title: 'ListItem2' }] }];
-        let list = new List('List body', 'btnText', sections, 'Title', 'footer');
-        client.sendMessage(msg.from, list);
-    } else if (msg.body === '!reaction') {
-        msg.react('👍');
-    } else if (msg.body === '!edit') {
-        if (msg.hasQuotedMsg) {
-            const quotedMsg = await msg.getQuotedMessage();
-            if (quotedMsg.fromMe) {
-                quotedMsg.edit(msg.body.replace('!edit', ''));
-            } else {
-                msg.reply('I can only edit my own messages');
-            }
-        }
-    } else if (msg.body === '!updatelabels') {
-        const chat = await msg.getChat();
-        await chat.changeLabels([0, 1]);
-    } else if (msg.body === '!addlabels') {
-        const chat = await msg.getChat();
-        let labels = (await chat.getLabels()).map(l => l.id);
-        labels.push('0');
-        labels.push('1');
-        await chat.changeLabels(labels);
-    } else if (msg.body === '!removelabels') {
-        const chat = await msg.getChat();
-        await chat.changeLabels([]);
     }
 });
 
@@ -291,27 +94,52 @@ const phoneNumberFormatter = function (number) {
     return formatted;
 }
 
+const registrarLog = function (chanel = 'DEBUG', message, data = null) {
+    const filePath = join(__dirname, '/data/app.log');
+
+    let chanelName = chanel.toUpperCase();
+
+    let datetime = new Date().toISOString();
+
+    let row = `[${datetime}] app.${chanelName}: ${message}`;
+
+    if (data !== null) {
+        row += ` ${JSON.stringify(data, null, 0)}`;
+    }
+
+    row += '\n';
+
+    fs.appendFile(filePath, row, (err) => {
+        if (err) {
+            console.error('Erro ao registrar o log:', err);
+        }
+    });
+}
+
 app.post('/send-message', async function (req, res) {
     const number = phoneNumberFormatter(req.body.number);
     const message = req.body.message;
 
+    registrarLog('info', '[send-message] Request data receive', { number, message });
+
     const isRegisteredNumber = await checkRegisteredNumber(number);
 
     if (!isRegisteredNumber) {
+        registrarLog('error', '[send-message] The number is not registered', { number });
         return res.status(422).json({
             status: false,
             message: 'The number is not registered'
         });
     }
 
-    console.log(number, message);
-
     client.sendMessage(number, message).then(response => {
+        registrarLog('success', '[send-message] Message sent by whatsapp');
         res.status(200).json({
             status: true,
             response: response
         });
     }).catch(err => {
+        registrarLog('error', '[send-message] Error when message sent by whatsapp', { err });
         res.status(500).json({
             status: false,
             response: err
@@ -324,6 +152,8 @@ app.post('/send-media', async (req, res) => {
     const caption = req.body.caption;
     const fileUrl = req.body.file;
 
+    registrarLog('info', '[send-media] Request data receive', { number, caption, fileUrl });
+
     // const media = MessageMedia.fromFilePath('./image-example.png');
     // const file = req.files.file;
     // const media = new MessageMedia(file.mimetype, file.data.toString('base64'), file.name);
@@ -335,16 +165,20 @@ app.post('/send-media', async (req, res) => {
         return response.data.toString('base64');
     });
 
+    registrarLog('info', '[send-media] file converted to base64', { mimetype });
+
     const media = new MessageMedia(mimetype, attachment, 'Media');
 
     client.sendMessage(number, media, {
         caption: caption
     }).then(response => {
+        registrarLog('success', '[send-media] File sent by whatsapp');
         res.status(200).json({
             status: true,
             response: response
         });
     }).catch(err => {
+        registrarLog('error', '[send-media] Error when file sent by whatsapp', { err });
         res.status(500).json({
             status: false,
             response: err
@@ -353,5 +187,7 @@ app.post('/send-media', async (req, res) => {
 });
 
 server.listen(port, () => {
-    console.log('Server running at http://localhost:' + port);
+    let message = `Server running at http://localhost:${port}`;
+    console.log(message);
+    registrarLog('info', message);
 });
